f9194cde4998f8e3665f2a338b6a6ecfc8001d49
pcherian10 <pcherian10@gmail.com>
2019-06-19 15:00:02 -0400

    added passportjs and mongo keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     googleClientID: '525035719519-8mf9jee065ld5i89kivdnvh6pmv9kelp.apps.googleusercontent.com',
     googleClientSecret: 'kovHroYXPWYzdJWH1kYX-0g1',
-    mongoURI: 'mongodb+srv://pcdogg:kFR5eZPiGNmJ5um@emailify-b7gsx.mongodb.net/test?retryWrites=true'
+    mongoURI: 'mongodb+srv://pcherian10:hackathon19@pink-poncho-hackathon-19-aymt4.mongodb.net/test?retryWrites=true&w=majority'
 }

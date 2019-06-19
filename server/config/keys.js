ab8556ce8341f2d4e02493a262c92b13dc2b73d7
pcherian10 <pcherian10@gmail.com>
2019-06-19 15:39:38 -0400

    added authentication

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    googleClientID: '525035719519-8mf9jee065ld5i89kivdnvh6pmv9kelp.apps.googleusercontent.com',
-    googleClientSecret: 'kovHroYXPWYzdJWH1kYX-0g1',
+    googleClientID: '1006496988945-e4ka61s9b8h3s9japrhgkk81sb9569kj.apps.googleusercontent.com',
+    googleClientSecret: 'hM3FqWYbA0K9sK0heCjnvCu_',
     mongoURI: 'mongodb+srv://pcherian10:hackathon19@pink-poncho-hackathon-19-aymt4.mongodb.net/test?retryWrites=true&w=majority'
 }
